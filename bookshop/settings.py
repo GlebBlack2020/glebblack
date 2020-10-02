230c4a21a5fc734d94aaa1862107da9aee9c813a
glebb <glebblack2016@gmail.com>
2020-10-02 15:39:10 +0300

    add app

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'bookshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgres_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
--- managebook/admin.py
+++ managebook/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from managebook.models import Genre, Book
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ['title', 'published_date']
+    search_fields = ['title']
+    list_filter = ['published_date', 'author', 'genre']
+
+
+admin.site.register(Book, BookAdmin)
 admin.site.register(Genre)
 
